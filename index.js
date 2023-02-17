9c39c8597cecd62035a2f5cb1d8490366bc361db
tamal15 <tamal15-13243@diu.edu.bd>
2023-02-17 22:06:42 +0600

    update-code

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express")
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const ObjectId = require("mongodb").ObjectId;
+const { v4: uuidv4 } = require("uuid");
 require('dotenv').config();
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
+// app.use(express.urlencoded({ extended: true }));
+const SSLCommerzPayment = require('sslcommerz')
+// app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors())
 app.use(express.json())
@@ -29,6 +33,10 @@ async function run() {
         const buyerCollection = database.collection('buyerproducts');
         const branchCollection = database.collection('branchName');
         const contactCollection = database.collection('contact');
+        const paymentCollection = database.collection('paymentData');
+        const feedbacksCollection = database.collection('userfeedbacks');
+        const resturantCollection = database.collection('resturantProduct');
+        const adminUploadPizzaCollection = database.collection('adminPizza');
 
 
 
@@ -52,6 +60,29 @@ async function run() {
             const result = await userCollection.updateOne(filter, updateDoc, option)
             res.json(result)
         });
+
+          // update profile 
+
+    app.put('/updateUser', async(req,res)=>{
+        const user=req.body;
+        const query={email:user.email}
+        const updateDoc={
+            $set:{
+                address:user.address,
+                mobile:user.mobile
+            }
+        }
+        const result=await userCollection.updateOne(query,updateDoc);
+        res.json(result)
+    });
+
+     // user profile email 
+     app.get('/updateUser/:email', async(req,res)=>{
+        const email=req.params.email;
+        const query={email:email};
+        const result=await userCollection.findOne(query)
+        res.json(result)
+    });
 
 
         // database admin role 
@@ -90,7 +121,7 @@ async function run() {
         });
 
 
-        //    post product buyer 
+        //    post product admin burger
         app.post('/PostUploadBuyer', async (req, res) => {
             const user = req.body;
             const result = await buyerCollection.insertOne(user);
@@ -102,14 +133,79 @@ async function run() {
             res.json(result)
         });
 
+        // post product admin pizza 
+
+                //    post product admin burger
+                app.post('/PostUploadpizza', async (req, res) => {
+                    const user = req.body;
+                    const result = await adminUploadPizzaCollection.insertOne(user);
+                    res.json(result)
+                });
+        
+
+                app.get('/PostUploadpizza', async (req, res) => {
+                    const result = await adminUploadPizzaCollection.find({}).toArray()
+                    res.json(result)
+                });
+
+                app.get("/PostUploadpizza/:email", async (req, res) => {
+                    // const buyeremail=req.body.cartProducts.map((data)=>data.buyerEmail)
+                    console.log(req.params.email);
+                    const email = req.params.email;
+                    const result = await buyerCollection
+                        .find({ buyerEmail: email })
+                        .toArray();
+                    console.log(result)
+                    res.send(result);
+                });
+
+
+
+                // get burger product show 
+
+                app.get("/burgershows", async (req, res) => {
+                    const page = req.query.page;
+                    const size = parseInt(req.query.size);
+                    const query = req.query;
+                    delete query.page
+                    delete query.size
+                    Object.keys(query).forEach(key => {
+                        if (!query[key])
+                            delete query[key]
+                    });
+            
+                    if (Object.keys(query).length) {
+                        const cursor = buyerCollection.find(query, status = "approved");
+                        const count = await cursor.count()
+                        const allQuestions = await cursor.skip(page * size).limit(size).toArray()
+                        res.json({
+                            allQuestions, count
+                        });
+                    } else {
+                        const cursor = buyerCollection.find({
+                            // status: "approved"
+                        });
+                        const count = await cursor.count()
+                        const allQuestions = await cursor.skip(page * size).limit(size).toArray()
+            
+                        res.json({
+                            allQuestions, count
+                        });
+                    }
+            
+                });
+
 
         app.get('/UploadBuyerProduct/:id', async (req, res) => {
             const id = req.params.id;
             const result = await buyerCollection.findOne({ _id: ObjectId(id) });
             res.json(result)
         });
+
+
+
       
-        // get sharee 
+        // get burger
         app.get("/products", async (req, res) => {
             const page = req.query.page;
             const size = parseInt(req.query.size);
@@ -143,6 +239,27 @@ async function run() {
         });
 
 
+        // myorder show 
+
+        app.get("/myOrder/:email", async (req, res) => {
+            // const buyeremail=req.body.cartProducts.map((data)=>data.buyerEmail)
+            console.log(req.params.email);
+            const email = req.params.email;
+            const result = await paymentCollection
+              .find({ cus_email: email })
+              .toArray();
+            res.send(result);
+          });
+
+
+        //   myorder delete 
+
+        app.delete("/manageAllOrderDelete/:id", async (req, res) => {
+            const result = await paymentCollection.deleteOne({_id:ObjectId(req.params.id)});
+            res.send(result);
+          });
+
+
         //   details product 
         // app.get('/product', async(req,res)=>{
         //     const branchs=req.body
@@ -162,7 +279,7 @@ async function run() {
         app.post('/branch', async (req, res) => {
             const user = req.body;
             console.log(user)
-            const result = await branchCollection.insertOne(user);
+            const result = await resturantCollection.insertOne(user);
             // console.log(body)
             res.json(result);
 
@@ -190,14 +307,14 @@ async function run() {
             });
 
             if (Object.keys(query).length) {
-                const cursor = branchCollection.find(query, status = "approved");
+                const cursor = resturantCollection.find(query, status = "approved");
                 const count = await cursor.count()
                 const allData = await cursor.skip(page * size).limit(size).toArray()
                 res.json({
                     allData, count
                 });
             } else {
-                const cursor = branchCollection.find({
+                const cursor = resturantCollection.find({
                     // status: "approved"
                 });
                 const count = await cursor.count()
@@ -212,7 +329,7 @@ async function run() {
 
         app.get('/branch/:id', async (req, res) => {
             const id = req.params.id;
-            const result = await branchCollection.findOne({ _id: ObjectId(id) });
+            const result = await resturantCollection.findOne({ _id: ObjectId(id) });
             res.json(result)
         });
         app.get('/buyerDetailsproduct/:id', async (req, res) => {
@@ -245,6 +362,303 @@ async function run() {
             console.log(result)
             res.send(result);
         });
+
+        // admin burger and pizza show 
+
+        app.get("/admiinpizza", async (req, res) => {
+            const page = req.query.page;
+            const size = parseInt(req.query.size);
+            const query = req.query;
+            delete query.page
+            delete query.size
+            Object.keys(query).forEach(key => {
+                if (!query[key])
+                    delete query[key]
+            });
+
+            if (Object.keys(query).length) {
+                const cursor = adminUploadPizzaCollection.find(query, status = "approved");
+                const count = await cursor.count()
+                const allData = await cursor.skip(page * size).limit(size).toArray()
+                res.json({
+                    allData, count
+                });
+            } else {
+                const cursor = adminUploadPizzaCollection.find({
+                    // status: "approved"
+                });
+                const count = await cursor.count()
+                const allData = await cursor.skip(page * size).limit(size).toArray()
+
+                res.json({
+                    allData, count
+                });
+            }
+
+        });
+
+          // feedback 
+        app.post('/feedbacks', async(req,res) =>{
+            const user=req.body;
+          console.log(user)
+            // console.log(like)
+            const result=await feedbacksCollection.insertOne(user);
+            res.json(result)
+        });
+
+
+        // resturant name post 
+                app.post('/resturantservice', async(req,res) =>{
+            const user=req.body;
+          console.log(user);
+          
+            const result=await resturantCollection.insertOne(user);
+            res.json(result)
+        });
+
+         app.get('/resturantservice', async(req,res)=>{
+            const result=await resturantCollection.find({}).toArray()
+            res.json(result)
+        });
+
+         app.put('/service', async (req, res) => {
+        
+            console.log(req.body)
+            // const filter = { _id: ObjectId(req.params.id) };
+            const query={
+                BranchName:req.body.BranchName}
+            const options = { upsert: true };
+            // const data=req.body
+           
+               
+                    const updateDoc = { $push: { services: req.body } };
+                    const result = await resturantCollection.updateOne(query, updateDoc, options);
+                    res.json(result)
+                
+              
+
+
+    });
+
+
+    // customer order 
+
+    app.get("/my/:email", async (req, res) => {
+        // const buyeremail=req.body.emails.map((data)=>data.buyerEmail)
+        // console.log(emails)
+        // console.log(req.params.email);
+        const email = req.params.email;
+        console.log(email)
+        const result = await paymentCollection
+          .find({ emails: email })
+          .toArray();
+        res.send(result);
+      });
+
+      // upadate status for put api 
+ app.put('/updateStatus/:id', async(req,res)=>{
+    const id=req.params.id;
+    const updateDoc=req.body.status;
+    console.log(updateDoc)
+    console.log(updateDoc)
+    const filter={_id:ObjectId(id)}
+    const result=await paymentCollection.updateOne(filter,{
+        $set:{status:updateDoc}
+    })
+    res.json(result)
+});
+
+// Delete manage all product ----------
+app.delete("/manageAllOrderDelete/:id", async (req, res) => {
+    const result = await paymentCollection.deleteOne({_id:ObjectId(req.params.id)});
+    res.send(result);
+  });
+
+
+
+//   admin all product show 
+app.get("/adminShowproduct", async (req, res) => {
+        const page = req.query.page;
+        const size = parseInt(req.query.size);
+        const query = req.query;
+        delete query.page
+        delete query.size
+        Object.keys(query).forEach(key => {
+            if (!query[key])
+                delete query[key]
+        });
+
+        if (Object.keys(query).length) {
+            const cursor = buyerCollection.find(query, status = "approved");
+            const count = await cursor.count()
+            const allQuestions = await cursor.skip(page * size).limit(size).toArray()
+            res.json({
+                allQuestions, count
+            });
+        } else {
+            const cursor = buyerCollection.find({
+                // status: "approved"
+            });
+            const count = await cursor.count()
+            const allQuestions = await cursor.skip(page * size).limit(size).toArray()
+
+            res.json({
+                allQuestions, count
+            });
+        }
+
+    });
+
+
+    // admin update product 
+
+    app.get('/update/:id', async(req,res)=>{
+        const id=req.params.id;
+        const query={_id:ObjectId(id)};
+        const user=await buyerCollection.findOne(query)
+        res.json(user)
+    });
+
+
+    // update product 
+
+    app.put("/updateProduct/:id", async (req, res) => {
+
+        const id=req.params.id;
+        const updateUser=req.body
+        console.log(updateUser)
+        const filter={_id: ObjectId(id)};
+        const options={upsert:true};
+
+        const updateDoc={
+            $set:{
+                productName:updateUser.productName,
+                ProductPrice:updateUser.ProductPrice
+            }
+        }
+        const result=await buyerCollection.updateOne(filter,updateDoc,options);
+        console.log('uodateinf',id);
+        res.json(result)
+
+    });
+
+
+    // admin product delete 
+
+    app.delete('/deleteadmin/:id',async(req,res)=>{
+        const result= await buyerCollection.deleteOne({_id:ObjectId(req.params.id)});
+        res.json(result)
+    });
+
+
+        // ssl commerce init 
+
+        app.post('/init', async(req, res) => {
+            // console.log(req.body)
+            const email=req.body.cartProducts.map((data)=>data.buyerEmail)
+            const schedule=req.body.cartProducts.map((data)=>data.schedule)
+            const adminemail=req.body.cartProducts.map((data)=>data.adminEmail)
+            console.log(email)
+            console.log(schedule)
+            const data = {
+                emails:email,
+                admindata:adminemail,
+                total_amount: req.body.total_amount,
+                currency: req.body.currency,
+                tran_id: uuidv4(),
+                success_url: 'http://localhost:5000/success',
+                fail_url: 'http://localhost:5000/fail',
+                cancel_url: 'http://localhost:5000/cancel',
+                ipn_url: 'http://yoursite.com/ipn',
+                shipping_method: 'Courier',
+                product_name: "req.body.product_name",
+                product_category: 'Electronic',
+                product_profile: "req.body.product_profile",
+                cus_name: req.body.cus_name,
+                cus_email: req.body.cus_email,
+                date: req.body.date,
+                
+                status: req.body.status,
+                cartProducts: req.body.cartProducts,
+                // buyerDetails: req.body.email,
+                // buyerDetails: req.body.console.log(cartProducts),
+                product_image: "https://i.ibb.co/t8Xfymf/logo-277198595eafeb31fb5a.png",
+                cus_add1: req.body.cus_add1,
+                cus_add2: 'Dhaka',
+                cus_city: req.body.cus_city,
+                schedules: req.body.schedules,
+                purchase: req.body.purchase,
+                cus_state:  req.body.cus_state,
+                cus_postcode: req.body.cus_postcode,
+                cus_country: req.body.cus_country,
+                cus_phone: req.body.cus_phone,
+                cus_fax: '01711111111',
+                ship_name: 'Customer Name',
+                ship_add1: 'Dhaka',
+                ship_add2: 'Dhaka',
+                ship_city: 'Dhaka',
+                ship_state: 'Dhaka',
+                ship_postcode: 1000,
+                ship_country: 'Bangladesh',
+                multi_card_name: 'mastercard',
+                value_a: 'ref001_A',
+                value_b: 'ref002_B',
+                value_c: 'ref003_C',
+                value_d: 'ref004_D'
+            };
+            // insert order data into database 
+            const order=await paymentCollection.insertOne(data)
+            console.log(data)
+            const sslcommer = new SSLCommerzPayment(process.env.STORE_ID,process.env.STORE_PASSWORD,false) //true for live default false for sandbox
+            sslcommer.init(data).then(data => {
+                //process the response that got from sslcommerz 
+                //https://developer.sslcommerz.com/doc/v4/#returned-parameters
+                // console.log(data);
+                // res.redirect(data.GatewayPageURL)
+                if(data.GatewayPageURL){
+                    res.json(data.GatewayPageURL)
+                  }
+                  else{
+                    return res.status(400).json({
+                      message:'payment session failed'
+                    })
+                  }
+            });
+        });
+
+        app.post('/success',async(req,res)=>{
+            // console.log(req.body)
+            const order = await paymentCollection.updateOne({tran_id:req.body.tran_id},{
+                $set:{
+                  val_id:req.body.val_id
+                }
+            
+              })
+            res.status(200).redirect(`http://localhost:3000/success/${req.body.tran_id}`)
+            // res.status(200).json(req.body)
+        })
+        
+        app.post ('/fail', async(req,res)=>{
+            // console.log(req.body);
+          const order=await paymentCollection.deleteOne({tran_id:req.body.tran_id})
+            res.status(400).redirect('http://localhost:3000')
+          })
+          app.post ('/cancel', async(req,res)=>{
+            // console.log(req.body);
+            const order=await paymentCollection.deleteOne({tran_id:req.body.tran_id})
+            res.status(200).redirect('http://localhost:3000')
+          })
+        
+        
+          app.get('/orders/:tran_id', async(req,res)=>{
+            const id=req.params.tran_id;
+            const order =await paymentCollection.findOne({tran_id:id});
+            console.log(order)
+            res.json(order)
+          });
+        
+        
+        
 
 
     }
