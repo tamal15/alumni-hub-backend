67fe001af6ca9964e996874a70404a3ab0585b12
tamal15 <tamal15-13243@diu.edu.bd>
2024-06-30 23:47:44 +0600

    alumnihubnewdata

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const ObjectId = require("mongodb").ObjectId;
 const { v4: uuidv4 } = require("uuid");
 require('dotenv').config();
+const bodyParser = require('body-parser');
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,9 +11,9 @@ const port = process.env.PORT || 5000;
 const SSLCommerzPayment = require('sslcommerz')
 // app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(cors())
 app.use(express.json())
-
 // projectdefence9 
 // yVWKJRh1rYivqCfF 
 
@@ -29,7 +30,6 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
-
 async function run() {
 
     try {
@@ -48,6 +48,12 @@ async function run() {
         const projectCollection = database.collection('project');
         const noteCollection = database.collection('note');
         const shareCollection = database.collection('sharePost');
+        const allysCollection = database.collection('ally');
+        const messageCollection = database.collection('message');
+        const addfriendCollection = database.collection('addfriend');
+        const groupdataCollection = database.collection('groupdata');
+        let groupCollection = database.collection('Groups');
+        let addgroupsCollection = database.collection('addgroups');
 
 
 
@@ -60,6 +66,64 @@ async function run() {
             res.json(result);
         });
 
+        // post message 
+        app.post('/messages', async (req, res) => {
+            const user = req.body;
+            console.log(user)
+            const result = await messageCollection.insertOne(user);
+            // console.log(body)
+            res.json(result);
+        });
+        // post group data
+        app.post('/groupdata', async (req, res) => {
+            const user = req.body;
+            console.log(user)
+            const result = await groupdataCollection.insertOne(user);
+            // console.log(body)
+            res.json(result);
+        });
+        // post add group data
+        app.post('/addgroupdata', async (req, res) => {
+            const user = req.body;
+            console.log(user)
+            const result = await addgroupsCollection.insertOne(user);
+            // console.log(body)
+            res.json(result);
+        });
+        // post add friend
+        app.post('/addfriend', async (req, res) => {
+            const user = req.body;
+            console.log(user)
+            const result = await addfriendCollection.insertOne(user);
+            // console.log(body)
+            res.json(result);
+        });
+
+
+        // delete add friend 
+        app.delete('/deleteManage/:id', async(req,res)=>{
+            const result=await addfriendCollection.deleteOne({_id:ObjectId(req.params.id)});
+            res.json(result)
+        })
+        app.get('/friendstatus', async (req, res) => {
+            const { userEmail, friendEmail } = req.query;
+            
+            try {
+              const status = await addfriendCollection.findOne({
+                email: userEmail,
+                friendEmail: friendEmail
+              });
+              
+              if (status) {
+                res.json(status.status);
+              } else {
+                res.json('no request');
+              }
+            } catch (error) {
+              res.status(500).send({ message: error.message });
+            }
+          });
+
         app.get('/userdata', async (req, res) => {
             
             const result = await userCollection.find({}).toArray()
@@ -67,6 +131,134 @@ async function run() {
             res.json(result);
 
         })
+        app.get('/getgroups', async (req, res) => {
+            
+            const result = await groupdataCollection.find({}).toArray()
+            // console.log(body)
+            res.json(result);
+
+        })
+        app.get('/showrequest', async (req, res) => {
+            
+            const result = await addfriendCollection.find({}).toArray()
+            // console.log(body)
+            res.json(result);
+
+        })
+
+        app.put('/updateFriendStatus/:id', async (req, res) => {
+            const { id } = req.params;
+            const { status } = req.body;
+            
+            try {
+              const result = await addfriendCollection.updateOne(
+                { _id: ObjectId(id) },
+                { $set: { status: status } }
+              );
+              res.json(result);
+            } catch (error) {
+              res.status(500).send({ message: error.message });
+            }
+          });
+
+        // get message 
+        app.get('/getmessage', async (req, res) => {
+            
+            const result = await messageCollection.find({}).toArray()
+            // console.log(body)
+            res.json(result);
+
+        })
+
+      // GET all groups
+      // GET all groups
+
+      let groups = [];
+
+      app.post('/create-group', (req, res) => {
+          const { name, user } = req.body;
+          const newGroup = { id: groups.length + 1, name, members: [user], posts: [] };
+          groups.push(newGroup);
+          res.status(201).json(newGroup);
+      });
+      
+      app.post('/join-group', (req, res) => {
+          const { groupId, user } = req.body;
+          const group = groups.find(g => g.id === groupId);
+          if (group) {
+              if (!group.members.some(member => member.email === user.email)) {
+                  group.members.push(user);
+                  res.status(200).json({ message: 'Joined successfully' });
+              } else {
+                  res.status(400).json({ message: 'User already a member' });
+              }
+          } else {
+              res.status(404).json({ message: 'Group not found' });
+          }
+      });
+      
+      app.post('/create-post', (req, res) => {
+          const { groupId, post } = req.body;
+          const group = groups.find(g => g.id === groupId);
+          if (group) {
+              group.posts.push(post);
+              res.status(201).json(post);
+          } else {
+              res.status(404).json({ message: 'Group not found' });
+          }
+      });
+      
+      app.get('/groups', (req, res) => {
+          res.status(200).json(groups);
+      });
+      
+      app.get('/group/:id', (req, res) => {
+          const group = groups.find(g => g.id === parseInt(req.params.id, 10));
+          if (group) {
+              res.status(200).json(group);
+          } else {
+              res.status(404).json({ message: 'Group not found' });
+          }
+      });
+
+
+      app.put('/Singlegroup', async (req, res) => {
+        
+        console.log(req.body)
+        // const filter = { _id: ObjectId(req.params.id) };
+        const query={
+            type:req.body.type}
+        const options = { upsert: true };
+        // const data=req.body
+       
+           
+                const updateDoc = { $push: { services: req.body } };
+                const result = await groupdataCollection.updateOne(query, updateDoc, options);
+                res.json(result)
+            
+          
+
+
+});
+
+
+      
+
+
+         // details show admin product 
+     app.get('/userdatadetails/:id', async(req,res)=>{
+        const id=req.params.id
+        const query={_id:new ObjectId(id)}
+        const result=await userCollection.findOne(query)
+        res.json(result)
+    });
+         // details group data
+     app.get('/groupdatacollected/:id', async(req,res)=>{
+        const id=req.params.id
+        const query={_id:new ObjectId(id)}
+        const result=await groupdataCollection.findOne(query)
+        res.json(result)
+    });
 
         app.post('/users', async (req, res) => {
             const user = req.body;
@@ -96,12 +288,100 @@ async function run() {
         const updateDoc={
             $set:{
                 address:user.address,
-                mobile:user.mobile
+                mobile:user.mobile,
+                schoolName:user.schoolName,
+                degree:user.degree,
+                schoolyear:user.schoolyear,
+                collegeName:user.collegeName,
+                collegeDegree:user.collegeDegree,
+                collegeYear:user.collegeYear,
+                UniversityName:user.UniversityName,
+                Universitydegree:user.Universitydegree,
+                Universityear:user.Universityear,
+                schoolimg:user.schoolimg,
+                collegeimg:user.collegeimg,
+                universityimg:user.universityimg,
             }
         }
         const result=await userCollection.updateOne(query,updateDoc);
         res.json(result)
     });
+
+    // skill update 
+    // Update profile endpoint
+
+    app.put('/updateUserskill', async (req, res) => {
+        try {
+            const user = req.body;
+            const query = { email: user.email };
+            const updateDoc = {
+                $set: {
+                    displayName: user.displayName,
+                    skillone: user.skillone,
+                    skilltwo: user.skilltwo,
+                    skillthree: user.skillthree,
+                    skillfour: user.skillfour,
+                    skillfive: user.skillfive,
+                    skillsix: user.skillsix,
+                    skillseven: user.skillseven,
+                    address: user.address
+                }
+            };
+            const result = await userCollection.updateOne(query, updateDoc);
+            res.json(result);
+        } catch (error) {
+            console.error(error);
+            res.status(500).send('An error occurred while updating the user profile.');
+        }
+    });
+    
+
+    // newprofile update data 
+    app.put('/updateprofiledata', async (req, res) => {
+        try {
+            const user = req.body;
+            const query = { email: user.email };
+            const updateDoc = {
+                $set: {
+                    displayName: user.displayName,
+                    designation: user.designation,
+                    work: user.work,
+                    img:user.img,
+                    varsityInitial: user.varsityInitial,
+                    department: user.department
+                }
+            };
+            const result = await userCollection.updateOne(query, updateDoc);
+            res.json(result);
+        } catch (error) {
+            console.error(error);
+            res.status(500).send('An error occurred while updating the user profile.');
+        }
+    });
+    
+
+    // experience update 
+    app.put('/experirnceupdate', async (req, res) => {
+        try {
+            const user = req.body;
+            const query = { email: user.email };
+            const updateDoc = {
+                $set: {
+                    images: user.images,
+                    designations: user.designations,
+                    works: user.works,
+                    years: user.years,
+                    locations: user.locations
+                }
+            };
+            const result = await userCollection.updateOne(query, updateDoc);
+            res.json(result);
+        } catch (error) {
+            console.error(error);
+            res.status(500).send('An error occurred while updating the user profile.');
+        }
+    });
+    
 
               // update sharepost
 
@@ -129,6 +409,8 @@ async function run() {
         const result=await userCollection.findOne(query)
         res.json(result)
     });
+
+    
 
 
         // database admin role 
@@ -186,6 +468,11 @@ async function run() {
         app.post('/note', async (req, res) => {
             const user = req.body;
             const result = await noteCollection.insertOne(user);
+            res.json(result)
+        });
+        app.post('/postally', async (req, res) => {
+            const user = req.body;
+            const result = await allysCollection.insertOne(user);
             res.json(result)
         });
 
@@ -274,6 +561,49 @@ async function run() {
             }
 
         });
+
+
+         // get homedata
+         app.get("/newgroup", async (req, res) => {
+            const page = req.query.page;
+            const size = parseInt(req.query.size);
+            const query = req.query;
+            delete query.page
+            delete query.size
+            Object.keys(query).forEach(key => {
+                if (!query[key])
+                    delete query[key]
+            });
+
+            if (Object.keys(query).length) {
+                const cursor = groupdataCollection.find(query, status = "approved");
+                const count = await cursor.count()
+                const allData = await cursor.skip(page * size).limit(size).toArray()
+                res.json({
+                    allData, count
+                });
+            } else {
+                const cursor = groupdataCollection.find({
+                    // status: "approved"
+                });
+                const count = await cursor.count()
+                const allData = await cursor.skip(page * size).limit(size).toArray()
+
+                res.json({
+                    allData, count
+                });
+            }
+
+        });
+
+        // single data post 
+app.get("/mypost/:email", async (req, res) => {
+    const email = req.params.email;
+    console.log(email);
+    const result = await homePostCollection.find({ buyerEmail: email }).toArray();
+    res.send(result);
+});
+
 
 
         // get developer 
